aea18caeb0bb085640ba7979015e0804b8b46772
JulioModo <167862251+JulioModo@users.noreply.github.com>
2025-11-14 07:14:26 +0100

    Create computer.py
    Added and,or,xor functions

---
+++ computer.py
@@ -0,0 +1,17 @@
+def and(a,b):
+    if a and b:
+       return 1
+    else:
+       return 0
+
+def or(a,b):
+    if a or b:
+       return 1
+    else:
+       return 0
+
+def xor(a,b):
+    if a ^ b:
+       return 1 
+    else:
+       return 0 
