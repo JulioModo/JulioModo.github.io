06b2add66a39552424a0d16a9ebcf8ec31303fea
JulioModo <167862251+JulioModo@users.noreply.github.com>
2026-02-04 14:55:05 +0100

    Added V1.py

---
+++ Gestionnaire de mots de passe/V1.py
@@ -0,0 +1,32 @@
+import secrets
+letters = 'abcdefghijklmnopqrstuvwxyz'
+caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+specials = '&(-_éàç)~#{[|`^@]}*µù%!§:/;.,?'
+
+def ask_length(min_len=6, max_len=64):
+    while True:
+        s = input(f"Enter password length ({min_len}-{max_len}): ")
+
+        if not s.isdigit():
+            print("❌ Really man?")
+            continue
+
+        length = int(s)
+
+        if length < min_len:
+            print(f"❌ A bit short for a password, don't you think? At least {min_len} ...")
+            continue
+
+        if length > max_len:
+            print(f"❌ Woah there pal, a bit too long, no? Less than {max_len} is fine!")
+            continue
+
+        return length
+
+
+def create_password():
+    length = ask_length()
+    chars = letters + caps + numbers + specials
+    return "".join(secrets.choice(chars) for _ in range(length))
+  
